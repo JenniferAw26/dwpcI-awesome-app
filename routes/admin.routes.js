2b399fe618c308cba812c77cfa481ca7fb48aefd
Jennifer Aguado <108686161+JenniferAw26@users.noreply.github.com>
2024-11-13 05:06:49 +0000

    💊 Funciones Auxiliares

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,18 +1,20 @@
 // Importando el enrutador de express
 import { Router } from 'express';
+// Importando el gestor de rutas
+import path from 'path';
+import { ROOT_DIR } from '../helpers/paths.js';
+
 // Creando una instancia del enrutador de express
 const router = Router();
+
 // GET /add-product
 router.get('/add-product', (req, res, next) => {
   // Servimos el formulario
   console.log("📢 Sirviendo formulario...");
-  res.send(`
-  <form action="/add-product" method="POST">
-    <input type="text" name="title">
-    <button type="submit">Add product</button>
-  </form>
-  `);
+  console.log(`📢 ROOT_DIR: ${ROOT_DIR}`);
+  res.sendFile(path.join(ROOT_DIR, 'views','add-product.html'));
 });
+
 // POST /add-product
 router.post('/add-product', (req, res) => {
   // Realizaremos la extracción de
@@ -20,5 +22,6 @@ router.post('/add-product', (req, res) => {
   console.log(req.body);
   res.redirect('/');
 });
+
 // Exportando el enrutador admin
 export default router;
