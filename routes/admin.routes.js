02068299c4a0316e20bb34831e6559dc2fe74838
Jennifer Aguado <108686161+JenniferAw26@users.noreply.github.com>
2024-11-13 04:53:32 +0000

    ⭐ Corrigiendo Errores y Descargando Biblioteca

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,18 +1,18 @@
 // Importando el enrutador de express
 import { Router } from 'express';
-// Importando el gestor de rutas
-import path from 'path';
-
 // Creando una instancia del enrutador de express
 const router = Router();
-
 // GET /add-product
 router.get('/add-product', (req, res, next) => {
   // Servimos el formulario
   console.log("📢 Sirviendo formulario...");
-  res.sendFile(path.resolve('views','add-product.html'));
+  res.send(`
+  <form action="/add-product" method="POST">
+    <input type="text" name="title">
+    <button type="submit">Add product</button>
+  </form>
+  `);
 });
-
 // POST /add-product
 router.post('/add-product', (req, res) => {
   // Realizaremos la extracción de
@@ -20,6 +20,5 @@ router.post('/add-product', (req, res) => {
   console.log(req.body);
   res.redirect('/');
 });
-
 // Exportando el enrutador admin
 export default router;
--- routes/shop.routes.js
+++ routes/shop.routes.js
@@ -1,17 +1,16 @@
 // Importando el enrutador de express
 import { Router } from 'express';
-// Importando el gestor de rutas
-import path from 'path';
-
 // Creando una instancia del enrutador de express
 const router = Router();
-
 // GET /
 router.get('/', (req, res)=>{
   console.log("📢 Sirviendo la ruta '/'");
-  res.sendFile(path.resolve('views','shop.html'));
+  // Se contesta al server
+  res.send(`
+    <h1>Welcome to Express Js</h1>
+    <p>This is my awesome app! 😎</p>
+  `);
 });
-
 // GET /about
 router.get('/about', (req, res) => {
   console.log("📢 Sirviendo la ruta '/about'");
@@ -21,3 +20,4 @@ router.get('/about', (req, res) => {
     <p>App for Fullstack Web Dev Course I!</p>
   `);
 });
+export default router;
